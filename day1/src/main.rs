1727859c75c7bae3d86cb89cde186c8c6c48732d
Sam Potter <spotter1642@gmail.com>
2023-02-23 04:46:59 -0600

    Day 1: Use custom error with its own debug impl

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,27 @@ fn main() {
     println!("{input}");
 }
 
-fn read_input() -> Result<String, std::io::Error> {
-    std::fs::read_to_string("src/input.txt")
+struct PathedIoError {
+    path: String,
+    inner: std::io::Error,
+}
+
+impl std::fmt::Debug for PathedIoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("PathedIoError")
+            .field("path", &self.path)
+            .field("inner", &self.inner)
+            .finish()
+    }
+}
+
+fn read_input() -> Result<String, PathedIoError> {
+    let path = "src/foobar.txt";
+    match std::fs::read_to_string(path) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(PathedIoError { 
+            path: path.into(),
+            inner: e,
+        })
+    }
 }
