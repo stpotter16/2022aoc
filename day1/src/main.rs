b8ac4e3f8e24e6ef585490379f39f6fd7566ac98
Sam Potter <spotter1642@gmail.com>
2023-02-26 12:47:51 -0600

    Day 1: Use fs-error

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,24 +3,7 @@ fn main() {
     println!("{input}");
 }
 
-struct PathedIoError {
-    path: String,
-    inner: std::io::Error,
-}
-
-impl std::fmt::Debug for PathedIoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "for file {:?}: {}", self.path, self.inner)
-    }
-}
-
-fn read_input() -> Result<String, PathedIoError> {
+fn read_input() -> Result<String, std::io::Error> {
     let path = "src/foobar.txt";
-    match std::fs::read_to_string(path) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(PathedIoError { 
-            path: path.into(),
-            inner: e,
-        })
-    }
+    fs_err::read_to_string(path)
 }
