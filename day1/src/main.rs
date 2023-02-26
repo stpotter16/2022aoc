cb96ea10b50fc1e1e2bd26b890a6fbe199a267ea
Sam Potter <spotter1642@gmail.com>
2023-02-26 13:12:32 -0600

    Day 1: Loop through input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,16 +1,10 @@
-use color_eyre::eyre::Context;
-
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let input = read_input()?;
-    println!("{input}");
+    for line in include_str!("input.txt").lines() {
+        println!("Got line: {line}");
+    }
 
     Ok(())
 }
 
-fn read_input() -> color_eyre::Result<String> {
-    let path = "src/foobar.txt";
-    let input = std::fs::read_to_string(path).wrap_err("reading src/input.txt")?;
-    Ok(input)
-}
