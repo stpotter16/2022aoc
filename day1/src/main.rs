4dd2343e4424d0799136da08d6b59086c599f15d
Sam Potter <spotter1642@gmail.com>
2023-03-07 14:22:57 -0600

    Day 1: Use binary heap for part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,22 +1,35 @@
-use itertools::Itertools;
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use itertools::{Itertools, FoldWhile};
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let answer = include_str!("input.txt")
+    let mut group_sums = include_str!("input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
         .batching(|it| {
-            let mut sum = None;
-            while let Some(Some(v)) = it.next() {
-                sum = Some(sum.unwrap_or(0) + v);
-            }
-            sum
+            it.fold_while(None, |acc: Option<u64>, v| match v {
+                Some(v) => FoldWhile::Continue(Some(acc.unwrap_or_default() + v)),
+                None => FoldWhile::Done(acc),
+            })
+            .into_inner()
         })
-        .sorted_by_key(|&v| u64::MAX -v)
-        .take(3)
-        .sum::<u64>();
-        println!("{answer:?}");
+        .map(Reverse);
+
+    let mut heap = BinaryHeap::new();
+
+    for init in (&mut group_sums).take(3) {
+        heap.push(init);
+    }
+
+    for rest in group_sums {
+        heap.push(rest);
+        heap.pop();
+    }
+
+    let answer = heap.into_iter().map(|Reverse(v)| v).sum::<u64>();
+    println!("{answer:?}");
 
     Ok(())
 }
