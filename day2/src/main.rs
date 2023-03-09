26ceaddb7f254c55bad1743c6ef06e29cc990cea
Sam Potter <spotter1642@gmail.com>
2023-03-08 19:58:21 -0600

    Day 2: Implement our basic types

---
+++ day2/src/main.rs
@@ -0,0 +1,58 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Move {
+    Rock,
+    Paper,
+    Scissor
+}
+
+#[derive(Debug)]
+struct Round {
+    theirs: Move,
+    ours: Move,
+}
+
+impl TryFrom<char> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissor),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+        }
+    }
+}
+
+impl FromStr for Round {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let (Some(theirs), Some(' '), Some(ours), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got{s:?}"));
+        };
+
+        Ok(Self {
+            theirs: theirs.try_into()?,
+            ours: ours.try_into()?
+        })
+    }
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    for round in include_str!("input.txt")
+        .lines()
+        .map(|line| line.parse::<Round>())
+        {
+            let round = round?;
+            println!("{round:?}");
+        }
+
+    Ok(())
+}
+
