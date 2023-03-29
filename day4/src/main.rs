e8e18e9582f3fe32484511f75a44e203bed1b21c
Sam Potter <spotter1642@gmail.com>
2023-03-29 14:53:46 -0500

    Day 4: Use custom extension trait

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,5 +1,21 @@
-fn main() {
-    for i in 2..=4 {
-        dbg!(i);
+use std::ops::RangeInclusive;
+
+trait InclusiveRangeExt {
+    fn contains_range(&self, other: &Self) -> bool;
+}
+
+impl<T> InclusiveRangeExt for RangeInclusive<T>
+where
+    T: PartialOrd,
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
     }
 }
+
+fn main() {
+    dbg!((2..=4).contains_range(&(6..=8)));
+    dbg!((6..=8).contains_range(&(2..=4)));
+    dbg!((4..=6).contains_range(&(6..=6)));
+}
+
