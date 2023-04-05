a14df0d793afcde0f42c39dc11d43f3b9405668b
Sam Potter <spotter1642@gmail.com>
2023-04-05 04:39:02 -0500

    Day 4: Solve part 1 with sample input

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,13 @@
 use std::ops::RangeInclusive;
 
+use itertools::Itertools;
+
 trait InclusiveRangeExt {
     fn contains_range(&self, other: &Self) -> bool;
+
+    fn contains_or_is_contained(&self, other: &Self) -> bool {
+        self.contains_range(other) || other.contains_range(self)
+    }
 }
 
 impl<T> InclusiveRangeExt for RangeInclusive<T>
@@ -14,8 +20,23 @@ where
 }
 
 fn main() {
-    dbg!((2..=4).contains_range(&(6..=8)));
-    dbg!((6..=8).contains_range(&(2..=4)));
-    dbg!((4..=6).contains_range(&(6..=6)));
+    let redundant = include_str!("sample.txt")
+        .lines()
+        .map(|l| {
+            l.split(',')
+                .map(|range| {
+                    range
+                        .split('-')
+                        .map(|n| n.parse().expect("range start/end should be u32"))
+                        .collect_tuple::<(u32, u32)>()
+                        .map(|(start, end)| start..=end)
+                        .expect("each range should have a start and end")
+                })
+            .collect_tuple::<(_,_)>()
+            .expect("each line must have a pair of ranges")
+        })
+        .filter(|(a, b)| a.contains_or_is_contained(b))
+        .count();
+    dbg!(redundant);
 }
 
